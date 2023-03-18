33284190c811a5c2e5f77336bd2cdfb3da7658e7
henry antonio <henryantonio850@gmail.com>
2023-03-17 22:11:08 -0500

    se inicia proyecto EjemploNomina

---
+++ src/main/java/co/edu/sena/adsi/jpa/entities/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package co.edu.sena.adsi.jpa.entities;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
